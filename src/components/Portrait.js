01ad92bb0288731aec483d338bfcf15ad264a201
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-06-30 23:05:56 +0530

    incoming call css effect

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import './Portrait.css'
 
 import { Avatar } from '@material-ui/core';
 
 function Portrait(props) {
     return (
         <>
-            <Avatar src={props.src}
-                className="shadow-lg" style={{ width:"150px",  height:"150px" }} 
-            />
+            <div style={{ position: 'relative'}}>
+                <div class="circle1"></div>
+                <Avatar src={props.src}
+                    className="shadow-lg" style={{ width:"150px",  height:"150px" }} 
+                />
+            </div>
             <h4 className="text-lg mt-4">{props.name}</h4>
             <p className="text-base text-gray-600">{props.role}</p>
             <p className="text-sm text-gray-400">{props.status}</p>
